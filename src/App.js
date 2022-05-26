d1b9c0a9a5469fcfbb5deb32233f8e24f89e67cb
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-05-26 09:13:00 -0500

    Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -23,13 +23,29 @@ function App() {
 
 	if (!searchValue.length >= 1) {
 		searchedTodos = todos;
-	  } else {
+	} else {
 		searchedTodos = todos.filter(todo => {
-		  const todoText = todo.text.toLowerCase();
-		  const searchText = searchValue.toLowerCase();
-		  return todoText.includes(searchText);
+			const todoText = todo.text.toLowerCase();
+			const searchText = searchValue.toLowerCase();
+			return todoText.includes(searchText);
 		});
-	  }
+	}
+
+
+	const completeTodo = (text) => {
+		const todoIndex = todos.findIndex(todo => todo.text === text);
+		const newTodos = [...todos];
+		newTodos[todoIndex].completed = true;
+		setTodos(newTodos);
+	};
+
+
+	const deleteTodo = (text) => {
+		const todoIndex = todos.findIndex(todo => todo.text === text);
+		const newTodos = [...todos];
+		newTodos.splice(todoIndex, 1);
+		setTodos(newTodos);
+	};
 
 	return (
 		<React.Fragment>
@@ -42,7 +58,15 @@ function App() {
 				setSearchValue={setSearchValue}
 			/>
 			<TodoList>
-				{searchedTodos.map(todo =>(<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
+				{searchedTodos.map(todo =>(
+					<TodoItem
+						key={todo.text}
+						text={todo.text}
+						completed={todo.completed}
+						onComplete={ () => completeTodo(todo.text) }
+						onDelete={ () => deleteTodo(todo.text) }
+					/>
+				))}
 			</TodoList>
 			<CreateTodoButtom />
 		</React.Fragment>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './TodoItem.css';
 
-function TodoItem(props) {
-
-	const onComplete = () => {
-		alert('Ya completaste el todo ' + props.text);
-	};
-	const onDelete = () => {
-		alert('Borraste el todo ' + props.text);
-	};
+function TodoItem( props ) {
 
 	return (
 		<li className="TodoItem">
 			<span
 				className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-				onClick={onComplete}
+				onClick={props.onComplete}
 			>
 				√
 			</span>
@@ -23,7 +16,7 @@ function TodoItem(props) {
 			</p>
 			<span
 				className="Icon Icon-delete"
-				onClick={onDelete}
+				onClick={props.onDelete}
 			>
 				X
 			</span>
