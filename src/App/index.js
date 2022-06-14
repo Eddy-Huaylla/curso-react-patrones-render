b9dd303ad006fec9094090b60df3572ccd4ba81b
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-06-14 14:56:24 -0500

    Analizando la composición de Todo Machine

--- src/App/index.js
+++ src/App/index.js
@@ -1,64 +1,14 @@
 import React from "react";
-/* import { TodoProvider } from "../TodoContext"; */
+import { TodoProvider } from "../TodoContext";
 import './App.css';
-/* import { AppUI } from "./AppUI"; */
+import { AppUI } from "./AppUI";
 
 
-/* function App() {
+function App() {
 	return (
 		<TodoProvider>
 			<AppUI />
 		</TodoProvider>
-	);
-} */
-
-function App() {
-	const [ state, setState ] = React.useState( 'Estado react' )
-
-	return (
-		<React.Fragment>
-			<TodoHeader>
-				<TodoCounter />
-				<TodoSearch />
-			</TodoHeader>
-			<TodoList>
-				<TodoItem state={ state } />
-			</TodoList>
-		</React.Fragment>
-	);
-}
-
-function TodoHeader( { children } ) {
-	return (
-		<header>
-			{ children }
-		</header>
-	);
-}
-
-function TodoCounter() {
-	return (
-		<p>Todo Counter </p>
-	);
-}
-
-function TodoSearch() {
-	return (
-		<p>Todo Search</p>
-	);
-}
-
-function TodoList( { children } ) {
-	return (
-		<section>
-			{ children }
-		</section>
-	);
-}
-
-function TodoItem( { state } ) {
-	return (
-		<p>TodoItem: { state } </p>
 	);
 }
 
